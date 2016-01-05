c70aa863f4b4d3837458d7c5ebe6fa90f072b42f
thanh <truongthanh.bk56@gmail.com>
2016-01-05 15:37:25 +0700

    refs #49891_login_with_remembering

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ActiveRecord::Base
     format: {with: VALID_EMAIL_REGEX},
     uniqueness: {case_sensitive: false}
   validates :name,  presence: true, length: {maximum: 50}
-  validates :password, presence: true, length: {minimum: 6}
+  validates :password, presence: true, length: {minimum: 6}, allow_nil: true
 
   before_save {self.email = email.downcase}
   has_secure_password
