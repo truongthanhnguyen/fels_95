619a222218498f44f678087f61664e5dcb7113d6
thanh <truongthanh.bk56@gmail.com>
2016-01-06 14:35:24 +0700

    refs #49895 activate account

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,12 @@
 User.create! name:  "thanh", email: "tiensistorm@gmail.com",
-  password: "thanh1", password_confirmation: "thanh1"
+  password: "thanh1", password_confirmation: "thanh1", activated: true,
+  activated_at: Time.zone.now
 
-30.times do |n|
+35.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
   User.create! name:  name, email: email, password: password,
-    password_confirmation: password
+    password_confirmation: password, activated: true,
+    activated_at: Time.zone.now
 end
